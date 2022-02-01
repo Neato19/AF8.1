b0bb0a7a4b0b2d115a4a1169f9419240b6f59308
Iván Martínez Sánchez <ivan.martinezsanchez.alum@iescamp.es>
2022-02-01 13:47:58 +0100

    first commit

---
+++ script/date.en.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    
+    var WeekDays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    
+    var f = new Date();  //javascript
+    
+    var strDate = WeekDays[f.getDay()] + " - " + months[f.getMonth()] + " " + f.getDate() + ", " + f.getFullYear();
+    $('footer span').last().text(f.getFullYear());
+});
